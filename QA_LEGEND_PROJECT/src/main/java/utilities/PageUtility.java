411fdede82c055f8ebf8df692773ef44f00e5207
NOORA <NOORA@192.168.1.34>
2024-07-03 14:34:09 +0530

    utility update

--- QA_LEGEND_PROJECT/src/main/java/utilities/PageUtility.java
+++ QA_LEGEND_PROJECT/src/main/java/utilities/PageUtility.java
@@ -55,4 +55,60 @@ public class PageUtility {
 	public static void verifyFrameSwitchingUsingName(WebDriver driver, int index, String framename) {
 		driver.switchTo().frame(framename);
 	}
+
+	public static void verifyMouseRightClick(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
+		action.contextClick(element).build().perform();
+	}
+
+	public static void verifyMouseDoubleClick(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
+		action.doubleClick(element).build().perform();
+	}
+
+	public static void verifyMouseOver(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
+		action.moveToElement(element).build().perform();
+	}
+
+	public void verifyMoveToElement(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
+		action.moveToElement(element).build().perform();
+	}
+
+	public void verifyMoveByOffset(WebDriver driver, int xOffset, int yOffset) {
+		Actions action = new Actions(driver);
+		action.moveByOffset(xOffset, yOffset);
+	}
+
+	public void verifyMoveToElementOffset(WebDriver driver, WebElement element, int xOffset, int yOffset) {
+		Actions action = new Actions(driver);
+		action.moveToElement(element, xOffset, yOffset);
+	}
+
+	public void verifyMoveToLocation(WebDriver driver, WebElement element, int xCordinate, int yCordinate) {
+		Actions action = new Actions(driver);
+		action.moveToLocation(xCordinate, yCordinate);
+	}
+
+	public void verifyClickAndHold(WebDriver driver) {
+		Actions action = new Actions(driver);
+		action.clickAndHold();
+	}
+
+	public static void verifyMouseRelease(WebDriver driver) {
+		Actions action = new Actions(driver);
+		action.release();
+	}
+
+	public static void verifyDragAndDrop(WebDriver driver, WebElement drag, WebElement drop) {
+		Actions action = new Actions(driver);
+		action.dragAndDrop(drag, drop).build().perform();
+	}
+
+	public static void verifyDragAndDropOffset(WebDriver driver, WebElement drag, int xoffset, int yoffset) {
+		Actions action = new Actions(driver);
+		action.dragAndDropBy(drag, xoffset, yoffset).build().perform();
+	}
+
 }
